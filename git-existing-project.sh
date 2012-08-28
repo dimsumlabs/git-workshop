13460c2c226125dde87a9dc41dd746a32c9bac89
Mathis Antony <sveitser@gmail.com>
2012-08-29 01:21:16 +0800

    Add Script for Existing Repos

---
+++ git-existing-project.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# first create a github repo and copy the url
+# ./git-existing-project project-directory github-repo-url
+# 
+
+cd $1
+git init
+git add .
+git commit -m "Initial Commit"
+git remote add origin $2
+git push -u origin master
+
